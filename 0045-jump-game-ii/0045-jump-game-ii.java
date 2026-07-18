72a1739a897f8421d75f731b053e87b4f1101e2a
vishu goswami <vishugoswami756@gmail.com>
2026-07-18 19:58:03 +0530

    Time: 1 ms (99.73%), Space: 47.6 MB (8.73%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] nums) {
+
+        int end = 0;
+        int farthest = 0;
+
+        int jump = 0;
+
+        for(int i = 0 ; i < nums.length-1; i++){
+
+            
+            farthest = Math.max(farthest , i + nums[i]);
+
+            if(end == i){
+
+                jump++;
+
+                end = farthest;
+            }
+        }
+        return jump;
+    }
+}
