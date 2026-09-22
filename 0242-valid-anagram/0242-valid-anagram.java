40883fad6e27de2a1587a264f78d4c7a8215978f
vishu goswami <vishugoswami756@gmail.com>
2026-09-23 03:20:45 +0530

    Time: 6 ms (44.27%), Space: 44.2 MB (81.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+
+        int [] freq = new int [26];
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < freq.length;i++){
+
+            if(freq[i] != 0 ){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
